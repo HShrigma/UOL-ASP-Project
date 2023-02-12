b5e186acfa28a9a4d9ec728cc3c0e746eb1f814e
Hristo Stantchev <hristostantchev@abv.bg>
2023-02-12 22:56:14 +0200

    fixed index.js bug; instantiated routes pages

---
+++ Code/routes/search.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const assert = require("assert");
+const { randomInt } = require("crypto");
+const { urlencoded } = require("express");
+const { check, validationResult } = require('express-validator');
+const e = require("express");
+const { exists } = require("fs");
+let objId = 0;
+
+module.exports = router;
