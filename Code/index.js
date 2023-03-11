0c0d8e5c024d941b65358ca5999e2943b6aad5e2
Hristo Stantchev <hristostantchev@abv.bg>
2023-03-11 15:31:55 +0200

    added new db to stay

--- Code/index.js
+++ Code/index.js
@@ -16,7 +16,15 @@ const searchRoutes = require('./routes/search');
 
 // DB Connection
 const sqlite3 = require('sqlite3').verbose();
-global.db = new sqlite3.Database('DB/pets.sqlite3');
+global.db = new sqlite3.Database('DB/pets.db',function(err){
+  if(err){
+    console.error(err);
+    process.exit(1); //Bail out we can't connect to the DB
+  }else{
+    console.log("Database connected");
+    global.db.run("PRAGMA foreign_keys=ON"); //This tells SQLite to pay attention to foreign key constraints
+  }
+});
 
 //view engine Setup
 app.set("views",__dirname + "/views");
