0bd123f4e5e677a7230da94e0f80e1be2e66930e
Hristo Stantchev <hristostantchev@abv.bg>
2023-03-11 15:56:18 +0200

    separate dB did not work, removed from project

--- Code/index.js
+++ Code/index.js
@@ -16,15 +16,7 @@ const searchRoutes = require('./routes/search');
 
 // DB Connection
 const sqlite3 = require('sqlite3').verbose();
-global.db = new sqlite3.Database('DB/pets.db',function(err){
-  if(err){
-    console.error(err);
-    process.exit(1); //Bail out we can't connect to the DB
-  }else{
-    console.log("Database connected");
-    global.db.run("PRAGMA foreign_keys=ON"); //This tells SQLite to pay attention to foreign key constraints
-  }
-});
+global.db = new sqlite3.Database('DB/pets.sqlite3');
 
 //view engine Setup
 app.set("views",__dirname + "/views");
